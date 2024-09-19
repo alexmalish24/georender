db94549eedec130119cad15e8d50fd39cb8e47c4
Malish Alex <alexmalish.ma@gmail.com>
2024-09-19 15:06:20 +0200

    added vector to Map class

--- georender/georender.py
+++ georender/georender.py
@@ -21,3 +21,36 @@ class Map(ipyleaflet.Map):
             
     def add_layer_control(self, position = 'topright'):
         self.add_control(ipyleaflet.LayersControl(position = position))
+
+    def add_geojson(self, data, name = 'geojson', **kwargs):
+
+        import json
+
+        if isinstance(data, str):
+            with open(data) as f:
+                data = json.load(f)
+        
+        if 'style' not in kwargs:
+            kwargs['style'] = {"color": "blue", "weight": 1, "fillColor": "blue", "fillOpacity": 0}
+
+        if 'hover_style' not in kwargs:
+            kwargs['hover_style'] = {"fillColor": "blue", "fillOpacity": 0.8}
+
+        layer = ipyleaflet.GeoJSON(data = data, name = name, **kwargs)
+        self.add(layer)
+
+    def add_shp(self, data, name = 'shp', **kwargs):
+
+        import geopandas as gpd
+
+        if isinstance(data, str):
+            data = gpd.read_file(data)
+
+        if 'style' not in kwargs:
+            kwargs['style'] = {"color": "blue", "weight": 1, "fillColor": "blue", "fillOpacity": 0}
+
+        if 'hover_style' not in kwargs:
+            kwargs['hover_style'] = {"fillColor": "blue", "fillOpacity": 0.8}
+
+        layer = ipyleaflet.GeoData(geo_dataframe = data, name = name, **kwargs)
+        self.add(layer)
